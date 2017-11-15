6914b4203ab7c632bf0bbe8a8674616d5cecc934
kamephis <kamephis@gmail.com>
2017-11-15 18:25:48 +0100

    + Added customizer settings for social media accounts

--- 404.php
+++ 404.php
@@ -1,6 +1,12 @@
 <?php get_header();?>
-<div class="col-lg-12">
-	<div class="alert alert-info">
+
+    <!--<h1 class="page-title"><?php _e('Not Found', 'sfkvs'); ?></h1>-->
+    <img src="/assets/img/404_11.png"/>
+
+
+    <div class="col-lg-12">
+
+        <div class="alert alert-info">
 		<h2>FEHLER 404</h2>
 		<h3>Ups, da ist etwas schief gelaufen, unter der aufgerufenen Adresse ist niemand zuhause.</h3>
 	</div>
--- navbar_top.php
+++ navbar_top.php
@@ -1,4 +1,4 @@
-<nav class="navbar sticky-top navbar-toggleable-md navbar-light" role="navigation" id="navbar-top">
+<nav class="navbar sticky-top navbar-toggleable-md navbar-light" id="navbar-top">
     <div class="container">
         <?php
         $custom_logo_id = get_theme_mod('custom_logo');
@@ -7,7 +7,7 @@
         echo '<a href="' . get_home_url() . '" title="Zurück zur Startseite">';
 
         if (has_custom_logo()) {
-            echo '<img src="' . esc_url($logo[0]) . '" class="img img-fluid">';
+            echo '<img src="' . esc_url($logo[0]) . '" class="img img-fluid" alt="SfK-Logo">';
         } else {
             echo '<h1>' . get_bloginfo('name') . '</h1>';
         }
@@ -28,82 +28,27 @@
                     'depth' => 2,
                     'container' => 'div',
                     'container_class' => 'navbar navbar-light',
-                    'container_id' => 'navcol-1',
+                    'container_id' => 'navcol_1',
                     'menu_class' => 'nav navbar-nav',
                     'fallback_cb' => 'WP_Bootstrap_Navwalker::fallback',
                     'walker' => new WP_Bootstrap_Navwalker())
             );
             ?>
 
-            <?php
-            wp_nav_menu(array(
-                    'theme_location' => 'top_right',
-                    'depth' => 2,
-                    'container' => 'div',
-                    'container_class' => 'navbar navbar-light',
-                    'container_id' => 'navcol-2',
-                    'menu_class' => 'nav navbar-nav btn-nav',
-                    'fallback_cb' => 'WP_Bootstrap_Navwalker::fallback',
-                    'walker' => new WP_Bootstrap_Navwalker())
-            );
-            ?>
+            <div class=" hidden-md-down">
+                <?php
+                wp_nav_menu(array(
+                        'theme_location' => 'top_right',
+                        'depth' => 2,
+                        'container' => 'div',
+                        'container_class' => 'navbar navbar-light  nav-button-right',
+                        'container_id' => 'navcol-2',
+                        'menu_class' => 'nav navbar-nav btn-nav',
+                        'fallback_cb' => 'WP_Bootstrap_Navwalker::fallback',
+                        'walker' => new WP_Bootstrap_Navwalker())
+                );
+                ?>
+            </div>
         </div>
     </div>
 </nav>
-
-<style>
-    #navbar-top {
-        display: block;
-        background: #fff;
-        opacity: 0.9;
-        border-bottom: 10px solid rgb(23, 27, 44);
-    }
-    .btn-nav a.nav-link {
-        color: white !important;
-    }
-    #navcol-2 ul li {
-
-        padding: 0.25rem 0.5rem;
-        font-size: 0.875rem;
-        border-radius: 0.2rem;
-        color: white !important;
-
-        background: #f0ad4e;
-        /*background: #5cb85c;*/
-
-        display: inline-block;
-        font-weight: normal;
-        line-height: 1.25;
-        text-align: center;
-        white-space: nowrap;
-        vertical-align: middle;
-        -webkit-user-select: none;
-        -moz-user-select: none;
-        -ms-user-select: none;
-        user-select: none;
-
-        -webkit-transition: all 0.2s ease-in-out;
-        -o-transition: all 0.2s ease-in-out;
-        transition: all 0.2s ease-in-out;
-    }
-
-    #navcol-2 ul li:focus, #navcol-2 ul li:hover {
-        text-decoration: none;
-    }
-
-    #navcol-2 ul li:a {
-        color: white !important;
-    }
-
-    /*
-        ul.nav li.dropdown:hover > ul.dropdown-menu {
-            display: block;
-        }
-
-        @media (min-width: 979px) {
-            ul.nav li.dropdown:hover > ul.dropdown-menu {
-                display: block;
-            }
-        }
-        */
-</style>
